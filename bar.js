29ab92cd469e50064654ffc924092a58d2420330
Victoria Kusuki <112574911+victoriakusuki@users.noreply.github.com>
2023-09-08 15:19:30 -0300

    Update bar.js

--- bar.js
+++ bar.js
@@ -1,11 +1,1 @@
-function multiplicar()
-{
- 
-    antes = parseFloat(document.getElementById ("antes").value)
-    depois = parseFloat(document.getElementById ("depois").value)
-    v= antes 
-    p = depois + (depois *0.10)
-    vf = p+v
-    alert ("O valor total é: "+vf)
 
-}
